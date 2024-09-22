51b656ce737a6abe0d9ddf2803d9678ea02936bb
PARTH SAARTHI <112106051+parth-saarthi@users.noreply.github.com>
2024-09-22 12:26:07 +0530

    Time: 25 ms (5.36%), Space: 45.8 MB (70.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,34 +1,32 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m=nums1.length;
-        int n=nums2.length;
+        int p=nums2.length;
         int k=0;
-        int[] arr=new int[m+n];
+        int[] arr=new int[m+p];
         for(int i=0;i<m;i++){
             arr[k++]=nums1[i];
         }
-        for(int j=0;j<n;j++){
+        for(int j=0;j<p;j++){
             arr[k++]=nums2[j];
         }
-        // int temp=0;
-        // int a=arr.length;
-        //  for(int i=0; i < n; i++){  
-        //          for(int j=1; j < (n-i); j++){  
-        //                   if(arr[j-1] > arr[j]){ 
-        //                          temp = arr[j-1];  
-        //                          arr[j-1] = arr[j];  
-        //                          arr[j] = temp;  
-        //                  }  
-        //          }  
-        //  } 
-        int a=arr.length;
-        Arrays.sort(arr);
+        int temp=0;
+        int n=arr.length;
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){ 
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                 }  
+         } 
         
-       if (a % 2 == 1){
-            return arr[a / 2];
+       if (n % 2 == 1){
+            return arr[n / 2];
         }
         else{
-        return (arr[(a / 2) - 1] + arr[a / 2]) / 2.0;
+        return (arr[(n / 2) - 1] + arr[n / 2]) / 2.0;
         }
         
     }
